3c4e2708be35bfef13dfdc487ba4c2589fba71a5
Stephen Johnson <srj15@case.edu>
2011-01-14 12:41:44 -0500

    Optimized line intersection

--- incontinents/util.py
+++ incontinents/util.py
@@ -15,8 +15,7 @@ def tuple_sub(a, b):
     return (a[0]-b[0], a[1]-b[1])
 
 def dot(v1,v2):
-    # http://stackoverflow.com/questions/1828233/optimized-dot-product-in-python
-    return sum(starmap(mul,izip(v1,v2)))
+    return v1[0]*v2[0]+v1[1]*v2[1]
 
 def point_inside_triangle(x, y, coord_list):
     # http://www.blackpawn.com/texts/pointinpoly/default.html
@@ -49,70 +48,38 @@ def area_of_triangle(tri):
     h = y2 - y1
     return w*h/2.0
 
-def intersect(p1, p2, p3, p4, allow_points=True):
-    if allow_points:
-        if p1.x == p3.x and p1.y == p3.y or \
-            p2.x == p4.x and p2.y == p4.y or \
-            p1.x == p4.x and p1.y == p4.y or \
-            p2.x == p3.x and p2.y == p3.y:
-            return False
+def _on_segment(xi, yi, xj, yj, xk, yk):
+  return (xi <= xk or xj <= xk) and (xk <= xi or xk <= xj) and \
+         (yi <= yk or yj <= yk) and (yk <= yi or yk <= yj)
+
+def _direction(xi, yi, xj, yj, xk, yk):
+    a = (xk - xi) * (yj - yi)
+    b = (xj - xi) * (yk - yi)
+    if a < b:
+        return -1
     else:
-        if p1.x == p3.x and p1.y == p3.y or \
-            p2.x == p4.x and p2.y == p4.y or \
-            p1.x == p4.x and p1.y == p4.y or \
-            p2.x == p3.x and p2.y == p3.y:
-            return True
-    """
-    min_x1, max_x1 = sorted([p1.x, p2.x])    
-    min_y1, max_y1 = sorted([p1.y, p2.y])
-    min_x2, max_x2 = sorted([p3.x, p4.x])    
-    min_y2, max_y2 = sorted([p3.y, p4.y])
-    if min_x1 >= max_x2: return False
-    if max_x1 <= min_x2: return False
-    if min_y1 >= max_y2: return False
-    if max_y1 <= min_y2: return False
-    """
-    xD1 = p2.x-p1.x
-    xD2 = p4.x-p3.x
-    yD1 = p2.y-p1.y
-    yD2 = p4.y-p3.y
-    xD3 = p1.x-p3.x
-    yD3 = p1.y-p3.y
-    
-    len1 = math.sqrt(xD1*xD1+yD1*yD1)
-    len2 = math.sqrt(xD2*xD2+yD2*yD2)
-    if len1 == 0:
-        print p1, p2
-    if len2 == 0:
-        print p3, p4
-    dot = (xD1*xD2+yD1*yD2)
-    if len1*len2 == 0:
-        deg = 0
-    else:
-        deg = dot/(len1*len2)
-    
-    if abs(deg) == 1:
+        if a > b:
+            return 1
+        else:
+            return 0
+
+def intersect(a, b, c, d):
+    if a.x == c.x and a.y == c.y or \
+        b.x == d.x and b.y == d.y or \
+        a.x == d.x and a.y == d.y or \
+        b.x == c.x and b.y == c.y:
         return False
-        
-    pt = primitives.Point(0,0);
-    div = yD2*xD1-xD2*yD1
-    if div == 0: return True
-    ua = (xD2*yD3-yD2*xD3)/div
-    ub = (xD1*yD3-yD1*xD3)/div
-    pt.x = p1.x+ua*xD1
-    pt.y = p1.y+ua*yD1
-    xD1 = pt.x-p1.x
-    xD2 = pt.x-p2.x
-    yD1 = pt.y-p1.y
-    yD2 = pt.y-p2.y
-    segmentLen1 = math.sqrt(xD1*xD1+yD1*yD1)+math.sqrt(xD2*xD2+yD2*yD2)
-    
-    xD1 = pt.x-p3.x
-    xD2 = pt.x-p4.x
-    yD1 = pt.y-p3.y
-    yD2 = pt.y-p4.y
-    segmentLen2 = math.sqrt(xD1*xD1+yD1*yD1)+math.sqrt(xD2*xD2+yD2*yD2);
-    
-    if abs(len1-segmentLen1) > 0.01 or abs(len2-segmentLen2) > 0.01:
-        return False
-    return True
+    x1, y1 = a[0], a[1]
+    x2, y2 = b[0], b[1]
+    x3, y3 = c[0], c[1]
+    x4, y4 = d[0], d[1]
+    d1 = _direction(x3, y3, x4, y4, x1, y1)
+    d2 = _direction(x3, y3, x4, y4, x2, y2)
+    d3 = _direction(x1, y1, x2, y2, x3, y3)
+    d4 = _direction(x1, y1, x2, y2, x4, y4)
+    return (((d1 > 0 and d2 < 0) or (d1 < 0 and d2 > 0)) and \
+          ((d3 > 0 and d4 < 0) or (d3 < 0 and d4 > 0))) or \
+         (d1 == 0 and _on_segment(x3, y3, x4, y4, x1, y1)) or \
+         (d2 == 0 and _on_segment(x3, y3, x4, y4, x2, y2)) or \
+         (d3 == 0 and _on_segment(x1, y1, x2, y2, x3, y3)) or \
+         (d4 == 0 and _on_segment(x1, y1, x2, y2, x4, y4))
