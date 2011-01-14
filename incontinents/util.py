3ca8e660b8bfd46055a4f1442e3a35e4b33a4d9b
Stephen Johnson <srj15@case.edu>
2011-01-14 11:52:53 -0500

    Removed dead code

--- incontinents/util.py
+++ incontinents/util.py
@@ -40,30 +40,6 @@ def point_inside_triangle(x, y, coord_list):
     
     return (u > 0) and (v > 0) and (u + v < 1)
 
-def point_inside_polygon(x,y,poly_list):
-    print poly_list
-    poly = []
-    for i in range(len(poly_list)/2):
-        poly.append((poly_list[i*2], poly_list[i*2+1]))
-    n = len(poly)
-    inside = False
-
-    p1x,p1y = poly[0]
-    if x == p1x and y == p1y: return False
-    for i in range(n+1):
-        p2x,p2y = poly[i % n]
-        if x == p2x and y == p2y: return False
-        if y > min(p1y,p2y):
-            if y <= max(p1y,p2y):
-                if x <= max(p1x,p2x):
-                    if p1y != p2y:
-                        xinters = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
-                    if p1x == p2x or x <= xinters:
-                        inside = not inside
-        p1x,p1y = p2x,p2y
-
-    return inside
-
 def area_of_triangle(tri):
     x1 = min(tri[0], tri[2], tri[4])
     y1 = min(tri[1], tri[3], tri[5])
