6505cd467d59639d7ac56c4a9a6c0ad7caabad7a
Stephen Johnson <srj15@case.edu>
2011-01-16 14:55:33 -0500

    Fixed horrific bug in point_inside_triangle, incidentally making it faster

--- incontinents/util.py
+++ incontinents/util.py
@@ -25,16 +25,17 @@ def dot(v1,v2):
 def point_inside_triangle(x, y, coord_list):
     # http://www.blackpawn.com/texts/pointinpoly/default.html
     
+    x1, x2, x3 = coord_list[0], coord_list[2], coord_list[4]
+    y1, y2, y3 = coord_list[1], coord_list[3], coord_list[5]
+    if x < x1 and x < x2 and x < x3: return False
+    if x > x1 and x > x2 and x > x3: return False
+    if y < y1 and y < y2 and y < y3: return False
+    if y > y1 and y > y2 and y > y3: return False
+    
     P = (x, y)
     A = coord_list[0:2]
     B = coord_list[2:4]
     C = coord_list[4:6]
-    x1, x2, x3 = A[0], B[0], C[0]
-    y1, y2, y3 = A[0], B[0], C[0]
-    if x < x1 and x < x2 and x < x3: return False
-    if x > x1 and x > x2 and x > x3: return False
-    if y < y1 and y < y2 and x < y3: return False
-    if y > y1 and y > y2 and x > y3: return False
     
     v0 = tuple_sub(C, A)
     v1 = tuple_sub(B, A)
